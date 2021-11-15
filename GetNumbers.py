0bde38130a8ff972045396bf483d56747f8d87f5
Taro Kimura <91779250+tarokimuraorg@users.noreply.github.com>
2021-11-15 14:47:09 +0900

    引数を減らした

--- GetNumbers.py
+++ GetNumbers.py
@@ -4,14 +4,11 @@ class GetNumbers:
     
     numbers = []
 
-    def __init__(self, number1, number2, number3, number4):
+    def __init__(self, numbers):
 
-        self.numbers.append(number1)
-        self.numbers.append(number2)
-        self.numbers.append(number3)
-        self.numbers.append(number4)
+        self.numbers = numbers
 
     def sort(self):
-        
+
         new_numbers = random.sample(self.numbers, 4)
         return new_numbers
