0b89c187064ed29386149c9a1fec536ad0f2d4af
Taro Kimura <tarokimuraorg@outlook.com>
2021-12-14 14:18:54 +0900

    入力する4つの数字がすべて0～9の範囲内であることを保証

--- GetNumbers.py
+++ GetNumbers.py
@@ -6,9 +6,24 @@ class GetNumbers:
 
     def __init__(self, number1, number2, number3, number4):
 
-        self.numbers = [number1,number2,number3,number4]
+        self.numbers = []
+
+        if (number1 >= 0 and number1 < 10):
+            self.numbers.append(number1)
+
+        if (number2 >= 0 and number2 < 10):
+            self.numbers.append(number2)
+
+        if (number3 >= 0 and number3 < 10):
+            self.numbers.append(number3)
+
+        if (number4 >= 0 and number4 < 10):
+            self.numbers.append(number4)
 
     def sort(self):
 
-        new_numbers = random.sample(self.numbers, 4)
-        return new_numbers
+        if (len(self.numbers) == 4):
+            return random.sample(self.numbers, 4)
+        else:
+            return []
+        
