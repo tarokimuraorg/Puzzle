00671882884ba839be2ed37ac260d928e1a602f6
Taro Kimura <tarokimuraorg@outlook.com>
2021-12-14 14:17:42 +0900

    入力する4つの数字がすべて0～9の範囲内であることを保証

--- GetFormulas.py
+++ GetFormulas.py
@@ -9,15 +9,18 @@ class GetFormulas:
         self.operators = operators
 
     def generate(self):
-        
-        formula1 = {"left":self.numbers[0], "right":self.numbers[1], "operator":self.operators[0]}
-        formula2 = {"left":self.numbers[1], "right":self.numbers[2], "operator":self.operators[1]}
-        formula3 = {"left":self.numbers[2], "right":self.numbers[3], "operator":self.operators[2]}
 
         formulas = []
 
-        formulas.append(formula1)
-        formulas.append(formula2)
-        formulas.append(formula3)
+        if (len(self.numbers) == 4):
+        
+            formula1 = {"left":self.numbers[0], "right":self.numbers[1], "operator":self.operators[0]}
+            formula2 = {"left":self.numbers[1], "right":self.numbers[2], "operator":self.operators[1]}
+            formula3 = {"left":self.numbers[2], "right":self.numbers[3], "operator":self.operators[2]}
+
+            formulas.append(formula1)
+            formulas.append(formula2)
+            formulas.append(formula3)
 
         return formulas
+    
